7779164c1844765f7eb77259d7dffe3ccea1792b
jojianp <jnpires2020@gmail.com>
2025-03-29 09:40:40 -0300

    Removendo algumas coisas do Usuario

--- code/aluguel-veiculos/src/main/java/br/com/lds/aluguel_veiculos/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lds.aluguel_veiculos.repositories;
-
-import br.com.lds.aluguel_veiculos.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByNome(String nome);
-}
