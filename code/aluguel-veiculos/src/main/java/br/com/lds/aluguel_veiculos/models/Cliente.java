29072b3a5c5fc762c92369c0525d80f595794092
SaulimDoPvP <SaulimDoPvP@gmail.com>
2025-04-05 16:13:21 -0300

    Toma front bonitinho de aluguel

--- code/aluguel-veiculos/src/main/java/br/com/lds/aluguel_veiculos/models/Cliente.java
+++ code/aluguel-veiculos/src/main/java/br/com/lds/aluguel_veiculos/models/Cliente.java
@@ -1,26 +1,26 @@
-package br.com.lds.aluguel_veiculos.models;
+    package br.com.lds.aluguel_veiculos.models;
 
-import jakarta.persistence.*;
-import lombok.*;
-import java.util.List;
+    import jakarta.persistence.*;
+    import lombok.*;
+    import java.util.List;
 
-@Data
-@Entity
-public class Cliente extends Usuario {
-    public static final int MAX_RENDIMENTOS = 3;
-    
-    @Column(unique = true, nullable = false)
-    private String cpf;
-    
-    @Column(unique = true, nullable = false)
-    private String rg;
-    
-    private String profissao;
-    private String endereco;
-    
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Rendimento> rendimentos;
+    @Data
+    @Entity
+    public class Cliente extends Usuario {
+        public static final int MAX_RENDIMENTOS = 3;
+        
+        @Column(unique = true, nullable = false)
+        private String cpf;
+        
+        @Column(unique = true, nullable = false)
+        private String rg;
+        
+        private String profissao;
+        private String endereco;
+        
+        @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<Rendimento> rendimentos;
 
-    @OneToMany(mappedBy = "proprietario", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Automovel> automoveis;
-}
+        @OneToMany(mappedBy = "proprietario", cascade = CascadeType.REMOVE, orphanRemoval = true)
+        private List<Automovel> automoveis;
+    }
